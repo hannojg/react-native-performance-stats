28285b0ac091299d105437778003d834266da30d
Hanno J. Gödecke <die.drei99@yahoo.de>
2022-06-06 17:48:27 +0200

    wip(ios): implemented memory usage

--- ios/PerformanceStats/PerformanceStats.mm
+++ ios/PerformanceStats/PerformanceStats.mm
@@ -1,11 +1,34 @@
 #import "PerformanceStats.h"
+#import <mach/mach.h>
+#import <React/RCTBridge+Private.h>
+#import <React/RCTBridge.h>
+#import <React/RCTUIManager.h>
 
 // Thanks to this guard, we won't import this header when we build for the old architecture.
 #ifdef RCT_NEW_ARCH_ENABLED
 #import "RNPerformanceStatsSpec.h"
 #endif
 
-@implementation PerformanceStats
+// NOTICE: Mainly copied from here: https://github.com/facebook/react-native/blob/main/React/CoreModules/RCTPerfMonitor.mm
+
+
+static vm_size_t RCTGetResidentMemorySize(void)
+{
+  vm_size_t memoryUsageInByte = 0;
+  task_vm_info_data_t vmInfo;
+  mach_msg_type_number_t count = TASK_VM_INFO_COUNT;
+  kern_return_t kernelReturn = task_info(mach_task_self(), TASK_VM_INFO, (task_info_t)&vmInfo, &count);
+  if (kernelReturn == KERN_SUCCESS) {
+    memoryUsageInByte = (vm_size_t)vmInfo.phys_footprint;
+  }
+  return memoryUsageInByte;
+}
+
+@implementation PerformanceStats {
+    bool _isRunning;
+}
+
+@synthesize bridge = _bridge;
 
 RCT_EXPORT_MODULE(PerformanceStats)
 
@@ -24,14 +47,48 @@ RCT_EXPORT_MODULE(PerformanceStats)
   return @[ @"performanceStatsUpdate" ];
 }
 
+- (void)updateStats
+{
+    // View count
+    NSDictionary<NSNumber *, UIView *> *views = [_bridge.uiManager valueForKey:@"viewRegistry"];
+    NSUInteger viewCount = views.count;
+    NSUInteger visibleViewCount = 0;
+    for (UIView *view in views.allValues) {
+      if (view.window || view.superview.window) {
+        visibleViewCount++;
+      }
+    }
+    
+    // Memory
+    double mem = (double)RCTGetResidentMemorySize() / 1024 / 1024;
+    
+    [self sendEventWithName:@"performanceStatsUpdate" body:@{
+        @"jsFps": @60,
+        @"uiFps": @120,
+        @"usedRam": [NSNumber numberWithDouble:mem],
+        @"viewCount": [NSNumber numberWithUnsignedInteger:viewCount],
+        @"visibleViewCount": [NSNumber numberWithUnsignedInteger:visibleViewCount]
+    }];
+    
+    __weak __typeof__(self) weakSelf = self;
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+      __strong __typeof__(weakSelf) strongSelf = weakSelf;
+      if (strongSelf && strongSelf->_isRunning) {
+        [strongSelf updateStats];
+      }
+    });
+}
+
 RCT_REMAP_METHOD(start, withCpu:(NSInteger*)withCpu)
 {
-    [self sendEventWithName:@"performanceStatsUpdate" body:@{@"jsFps": @60, @"uiFps": @120}];
+    _isRunning = true;
+    [self updateStats];
+    
 }
 
 RCT_EXPORT_METHOD(stop)
 {
-    
+    _isRunning = false;
 }
 
 // Thanks to this guard, we won't compile this code when we build for the old architecture.
