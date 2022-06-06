6e98094c56c982c6217fee3e277fd3a4058e640b
Hanno J. Gödecke <die.drei99@yahoo.de>
2022-06-06 13:27:55 +0200

    feat(android): implemented listener for getting stats in JS

--- android/src/oldarch/java/nl/skillnation/perfstats/PerformanceStatsModule.java
+++ android/src/oldarch/java/nl/skillnation/perfstats/PerformanceStatsModule.java
@@ -29,4 +29,14 @@ public class PerformanceStatsModule extends ReactContextBaseJavaModule {
     public void stop() {
         performanceStats.stop();
     }
+
+    @ReactMethod
+    public void addListener(String eventName) {
+        // Set up any upstream listeners or background tasks as necessary
+    }
+
+    @ReactMethod
+    public void removeListeners(Integer count) {
+        // Remove upstream listeners, stop unnecessary background tasks
+    }
 }
